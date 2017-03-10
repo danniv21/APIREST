a2c0a49165a5c4e5b529ba4d9a629e4814ef6542
DHINOSTROZA <DHINOSTROZA@HINOSTROZAD>
2017-03-10 11:57:07 -0500

    update format code

--- 2.0.0/claro-ventas-linea/claro-rest-api-client/src/test/java/pe/com/claro/restclient/RestClientTest.java
+++ 2.0.0/claro-ventas-linea/claro-rest-api-client/src/test/java/pe/com/claro/restclient/RestClientTest.java
@@ -6,18 +6,17 @@ import org.junit.Test;
 
 import pe.com.claro.restclient.RestClient.RestClientBuilder;
 
-
 public class RestClientTest {
-	
+
 	@Test
-	public void  test01() {
-	   final String RESOURCE_URL = "http://localhost:7001";		
-		RestClient restClient = new RestClientBuilder(RESOURCE_URL).connectionTimeout(5000).build();	
-		 HashMap<String, String> querys = new HashMap<String, String>();
-		 querys.put("start", "0");
-		 querys.put("size", "3");
-	   String data= restClient.get("/claro-ventas-linea-resource/api/api-docs/clientes", String.class, querys);		
-	   System.out.println(data);   
+	public void test01() {
+		final String RESOURCE_URL = "http://localhost:7001";
+		RestClient restClient = new RestClientBuilder(RESOURCE_URL).connectionTimeout(5000).build();
+		HashMap<String, String> querys = new HashMap<String, String>();
+		querys.put("start", "0");
+		querys.put("size", "3");
+		String data = restClient.get("/claro-ventas-linea-resource/api/api-docs/clientes", String.class, querys);
+		System.out.println(data);
 	}
 
 }
